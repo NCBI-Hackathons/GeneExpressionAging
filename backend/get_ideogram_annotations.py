54cc5fa17c81aa3ad36e36bfe06602696e00726a
Eric Weitz <eric.m.weitz@gmail.com>
2017-08-15 08:49:31 -0400

    Refine annotations script

--- backend/get_ideogram_annotations.py
+++ backend/get_ideogram_annotations.py
@@ -1,7 +1,7 @@
 import json
 
-# Our longitudinal expression data provides 3 replicates for each age
-# This function averages those replicates
+# Our longitudinal expression data provides multiple replicates for each age
+# This function averages those replicates, combining "age" and "flu" components
 def get_mean_gene_expressions_over_time(tissue):
 
     with open(data_dir + tissue + '.cleaned.norm.counts.csv') as f:
@@ -19,10 +19,12 @@ def get_mean_gene_expressions_over_time(tissue):
         # See ../data/column_components.csv
         components = header.split('_')
         age = components[2]
-        if age in age_indexes_by_age:
-            age_indexes_by_age[age].append(i + 1)
+        flu = components[3]
+        age_flu = age + '_' + flu
+        if age_flu in age_indexes_by_age:
+            age_indexes_by_age[age_flu].append(i + 1)
         else:
-            age_indexes_by_age[age] = [i + 1]
+            age_indexes_by_age[age_flu] = [i + 1]
 
     for line in lines[1:]:
 
@@ -62,16 +64,35 @@ def get_gene_locations():
     return gene_locations
 
 
+# Sorts compound age_flu key by age, then flu
+# Example input:
+# ['01M_F0', '24M_F150', '24M_F10', '18M_F0', '18M_F10', '04M_F0', '04M_F10', '04M_F150', '09M_F150', '12M_F150', '12M_F0', '09M_F0', '12M_F10', '24M_F0', '18M_F150', '09M_F10']
+# Example output:
+# ['01M_F0', '04M_F0', '09M_F0', '12M_F0', '18M_F0', '24M_F0', '04M_F10', '09M_F10', '12M_F10', '18M_F10', '24M_F10', '04M_F150', '09M_F150', '12M_F150', '18M_F150', '24M_F150']
+def sort_age_flu(age_flu_list):
+
+    age_flu_list.sort(key=lambda x: int(x.split('_')[0][:-1]))
+    age_flu_list.sort(key=lambda x: int(x.split('_')[1][1:]))
+
+    return age_flu_list
+
+
 # @param gene_locations Dictionary of form "gene_id: {chromosome, start, end}"
 # @param expressions Dictionary mean gene expression by age; of form "gene_id: {age1, age2, ..., ageN}"
 def get_annotations(gene_locations, expressions):
 
     annotations = {}
 
-    for gene_id in list(expressions.keys())[:10]:
+    example_gene_id = list(expressions.keys())[0]
+    ages = expressions[example_gene_id]
+
+    # Example "age_flu": 18M_F150 (18 months, f150).  All ages are in months.
+    sorted_ages = sort_age_flu(list(ages.keys()))
+
+    for gene_id in list(expressions.keys()):
 
         if gene_id not in gene_locations:
-            print('Gene locations missing for ' + gene_id)
+            # print('Gene locations missing for ' + gene_id)
             continue
 
         location = gene_locations[gene_id]
@@ -81,8 +102,10 @@ def get_annotations(gene_locations, expressions):
 
         ordered_mean_expressions_by_age = []
 
-        for age in sorted(expressions):
-            ordered_mean_expressions_by_age.append(expressions[age])
+        expression = expressions[gene_id]
+
+        for age in sorted_ages:
+            ordered_mean_expressions_by_age.append(expression[age])
 
         annotation = [gene_id, start, length, 0]
         annotation += ordered_mean_expressions_by_age
@@ -93,9 +116,6 @@ def get_annotations(gene_locations, expressions):
             annotations[chromosome] = [annotation]
 
     keys = ['gene_id', 'start', 'length', 'traceIndex']
-
-    example_gene_id = list(expressions.keys())[0]
-    ages = sorted(expressions[example_gene_id])
 
     keys.extend(ages)
 
@@ -115,5 +135,5 @@ gene_locations = get_gene_locations()
 
 annotations = get_annotations(gene_locations, expressions)
 
-print(json.dumps(annotations['annotations'], indent=2))
+print(json.dumps(annotations, indent=2))
 
