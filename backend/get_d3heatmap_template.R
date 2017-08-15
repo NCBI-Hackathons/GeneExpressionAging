81c533ea5e795931f837fd4341b24470460e36bb
Shuaicheng Freeman Wang <shuaichengwang@gmail.com>
2017-08-15 15:58:27 -0400

    convert heatmap code into an reusable R function

--- backend/get_d3heatmap_template.R
+++ backend/get_d3heatmap_template.R
@@ -3,9 +3,12 @@
 
 
 ## libs
+library(data.table)
 library(plyr)
 library(dplyr)
+library(plotly)
 library(d3heatmap)
+library(htmlwidgets)
 options(tibble.width = Inf)
 
 # reference
@@ -14,40 +17,77 @@ serialize <- function(widget) {
       htmlwidgets:::toJSON2(widget, pretty=TRUE, digits = 12)
 }
 
-mock_d3heatmap_record <- function(...) {
-    cat(format(sys.call(0)), "\n")
-    d <- d3heatmap::d3heatmap(...)
-    json <- serialize(d$x)
-    cat(json, "\n")
+
+
+get_d3heatmap_template_and_json <- function(data, 
+                                            selected_features=NULL, 
+                                            selected_samples=NULL, 
+                                            k_row=3,
+                                            k_col=3, ...){
+
+    if (is.null(selected_samples)) {
+        selected_samples <- setdiff(colnames(data), 'ensembl')
+    }
+
+    if (is.null(selected_features)) {
+        selected_features <- data$ensembl
+    }
+
+    cor.data <- as.data.frame(data) %>%
+    dplyr::filter(ensembl %in% selected_features) %>%
+    dplyr::select(one_of(selected_samples)) %>%
+    as.matrix
+
+    #rownames(cor.data) <- selected_features
+    #colnames(cor.data) <- selected_samples
+
+    # for template usage, just randomly select 50 features
+    cor <- cor.data %>%
+    t %>%
+    cor(method='pearson')
+
+    colnames(cor) <- selected_features
+    rownames(cor) <- selected_features
+
+    # set 3 cluster both row-wise and col-wise for now
+    d <- d3heatmap::d3heatmap(cor, k_row=k_row, k_col=k_col)
+    json <- serialize(d)
+
+    return(list(d3heatmap.obj=d, json=json))
 }
 
 
+
+# example on using the function
 if (TRUE)  {     # load data for template
-    dat  <- fread(file.path('../data/lung.cleaned.log.counts.csv'), header=TRUE, stringsAsFactors=FALSE)
+    dat  <- fread(file.path('../data/lung.shuff.log.counts.csv'), header=TRUE, stringsAsFactors=FALSE)
     meta <- fread(file.path('../data/column_components.csv'), header=TRUE, stringsAsFactors=FALSE)
+    selected_features <- dat$ensembl[1:20]
+    selected_samples <- setdiff(colnames(data), 'ensembl')
 }    # End load data
 
+ret <- get_d3heatmap_template_and_json(dat, selected_features, selected_samples)
 
-# for template usage, just randomly select 50 features
-cor <- dat[1:50, ] %>% 
-dplyr::select(-ensembl) %>% 
-t %>%
-as.data.frame %>%
-cor(method='pearson')
+names(ret)
 
-colnames(cor) <- dat$ensembl[1:50]
-rownames(cor) <- dat$ensembl[1:50]
+write(ret$json, file=file.path('../webcomponents/src/corr-d3heatmap/template_corr_d3heatmap.json'))
 
-# set 3 cluster both row-wise and col-wise for now
-d <- d3heatmap::d3heatmap(cor, k_row=3, k_col=3)
-json <- serialize(d$x)
-json
-
-write(json, file=file.path('./webcomponents/src/corr-d3heatmap/template_corr_d3heatmap.json'))
-
-htmlwidgets::saveWidget(as_widget(d), 
-                        file.path("./webcomponents/src/corr-d3heatmap/template_corr_d3heatmap.html"), 
+htmlwidgets::saveWidget(as_widget(ret$d3heatmap.obj), 
+                        file.path("/home/freeman/github/GeneExpressionAging/webcomponents/src/corr-d3heatmap/template_corr_d3heatmap.html"), 
                         selfcontained=FALSE)
+                        #file.path('../webcomponents/src/corr-d3heatmap/template_corr_d3heatmap.html'), 
                         #file.path("/home/freeman/github/GeneExpressionAging/webcomponents/src/corr-d3heatmap/template_corr_d3heatmap.html"), 
 
+
+#----------------------------------------------------------------------------
+output_name <- 'test'
+`%+%` <- function(x, y) paste0(x, y)
+
+if (FALSE) {    # debug
+    data              <- dat
+    selected_features <- dat$ensembl[1:20]
+    selected_samples  <- setdiff(colnames(data), 'ensembl')
+    k_row             <- 3
+    k_col             <- 3
+}    # End 
 
