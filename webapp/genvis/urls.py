9d760dc30fe34f3148ea7b7cb60e7142a0e557cd
Eric Weitz <eric.m.weitz@gmail.com>
2017-08-15 23:02:52 -0400

    Use Ideogram.js to show distribution of genes across genome

--- webapp/genvis/urls.py
+++ webapp/genvis/urls.py
@@ -4,4 +4,5 @@ import genvis.views
 
 urlpatterns = [
     url(r'^test', genvis.views.TestView.as_view(), name="test"),
+    url(r'^ideogram', genvis.views.IdeogramView.as_view(), name="ideogram"),
 ]
